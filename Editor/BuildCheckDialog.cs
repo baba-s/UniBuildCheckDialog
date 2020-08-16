b4c9a40637f46a3b195345871dd8808982039314
baba_s <shotababa1206@gmail.com>
2020-08-16 10:45:23 +0900

    first commit

---
+++ Editor/BuildCheckDialog.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+
+namespace Kogane.Internal
+{
+	[InitializeOnLoad]
+	internal static class BuildCheckDialog
+	{
+		static BuildCheckDialog()
+		{
+			BuildPlayerWindow.RegisterBuildPlayerHandler( OnBuild );
+		}
+
+		private static void OnBuild( BuildPlayerOptions options )
+		{
+			var isBuild = EditorUtility.DisplayDialog
+			(
+				title: "Unity",
+				message: "ビルドを開始しますか？",
+				ok: "はい",
+				cancel: "いいえ"
+			);
+
+			if ( !isBuild ) return;
+
+			BuildPipeline.BuildPlayer( options );
+		}
+	}
+}
